38dba73adc498b93382fec2f8e4e804e642f0590
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-10-24 09:22:10 +0900

    a

--- kitamura_test.cpp
+++ kitamura_test.cpp
@@ -1,9 +1,18 @@
 #include "kitamura_test.h"
+#include "inputCtrl.h"
 
 Scene* kitamura_test::update()
 {
 
-
+    if (InputCtrl::GetKeyState(KEY_INPUT_1) == PRESS) {
+        wp.SetWeaponType(sword);
+    }
+    if (InputCtrl::GetKeyState(KEY_INPUT_2) == PRESS) {
+        wp.SetWeaponType(dagger);
+    }
+    if (InputCtrl::GetKeyState(KEY_INPUT_3) == PRESS) {
+        wp.SetWeaponType(greatSword);
+    }
     wp.Update();
 
     return this;
--- weapon.cpp
+++ weapon.cpp
@@ -11,10 +11,32 @@
 
 weapon::weapon()
 {
-	baseVec = { 100,0,100 };
-	
-
+	weaponType = none;
+	baseVec = { 80,0,80 };
 	relativeRot = 90.0f;		//武器によって変える
+	maxRot = 90.0f;
+	coolTime = 120;
+	isAttacking = false;
+}
+
+weapon::weapon(int type)
+{
+	//使うか不明
+	weaponType = type;
+	switch (weaponType)
+	{
+	case sword:
+		baseVec = { 80,0,80 };
+		break;
+
+	case dagger:
+		baseVec = { 50,0,50 };
+		break;
+
+	case greatSword:
+		baseVec = { 100,0,100 };
+		break;
+	}
 }
 
 weapon::~weapon()
@@ -36,16 +58,30 @@ void weapon::Update()
 		angle += M_PI;
 	}
 
+	//武器所有中なら
+	if (weaponType != none) {
+		//クールタイム中
+		if (!(isAttacking) && --coolTime <= 0) {
+			isAttacking = true;
+			coolTime = 120;
+		}
 
-	relativeRot -= 4.0f;
-	if (relativeRot < -90.0f) {
-		relativeRot = 90.0f;
+		//攻撃中
+		if (isAttacking) {
+
+			if (relativeRot < - maxRot) {
+				relativeRot = maxRot;
+				isAttacking = false;
+			}
+			rot = -1 * (angle - (d_r(relativeRot)));
+
+			collisionX = (baseVec.x * cos((rot)) - baseVec.y * sin((rot))) + 640;
+			collisionY = (baseVec.x * sin((rot)) + baseVec.y * cos((rot))) + 360;
+
+			relativeRot -= 4.0f;
+		}
+
 	}
-	rot = -1 * (angle - (d_r(relativeRot)));
-
-	collisionX = (baseVec.x * cos((rot)) - baseVec.y * sin((rot))) + 640;
-	collisionY = (baseVec.x * sin((rot)) + baseVec.y * cos((rot))) + 360;
-
 }
 
 void weapon::Draw() const
@@ -58,10 +94,36 @@ void weapon::Draw() const
 	DrawFormatString(0, 30, 0xffffff, "%f", baseVec.x);
 	DrawFormatString(0, 60, 0xffffff, "%f", baseVec.y);
 	DrawFormatString(0, 90, 0xffffff, "%f", rot);
-	DrawCircle(collisionX, collisionY, 3, 0xff0000, TRUE);
-	DrawCircle(640, 360, 3, 0xff0000, TRUE);
-	DrawLine(640, 360, collisionX, collisionY, 0xffffff);
+
+	if (isAttacking) {
+		DrawCircle(collisionX, collisionY, 3, 0xff0000, TRUE);
+		DrawLine(640, 360, collisionX, collisionY, 0xffffff);
+	}
 	
+
+	DrawCircle(640, 360, 3, 0xff0000, TRUE);
+}
+
+void weapon::SetWeaponType(int type)
+{
+	weaponType = type;
+	switch (weaponType)
+	{
+	case sword:
+		baseVec = { 80,0,80 };
+		maxRot = 60.0f;
+		break;
+
+	case dagger:
+		baseVec = { 50,0,50 };
+		maxRot = 60.0f;
+		break;
+
+	case greatSword:
+		baseVec = { 100,0,100 };
+		maxRot = 90.0f;
+		break;
+	}
 }
 
 //回転の公式
