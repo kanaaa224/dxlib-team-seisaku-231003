132c05eab86f9d759fbdbcd1da7e55824eb6ecc9
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-10-24 11:38:44 +0900

    add weapon level up system

--- kitamura_test.cpp
+++ kitamura_test.cpp
@@ -3,15 +3,16 @@
 
 Scene* kitamura_test::update()
 {
-
-    if (InputCtrl::GetKeyState(KEY_INPUT_1) == PRESS) {
-        wp.SetWeaponType(sword);
-    }
-    if (InputCtrl::GetKeyState(KEY_INPUT_2) == PRESS) {
-        wp.SetWeaponType(dagger);
-    }
-    if (InputCtrl::GetKeyState(KEY_INPUT_3) == PRESS) {
-        wp.SetWeaponType(greatSword);
+    if (!wp.GetLevelUpFlg()) {
+        if (InputCtrl::GetKeyState(KEY_INPUT_1) == PRESS) {
+            wp.SetWeaponType(sword);
+        }
+        if (InputCtrl::GetKeyState(KEY_INPUT_2) == PRESS) {
+            wp.SetWeaponType(dagger);
+        }
+        if (InputCtrl::GetKeyState(KEY_INPUT_3) == PRESS) {
+            wp.SetWeaponType(greatSword);
+        }
     }
     wp.Update();
 
