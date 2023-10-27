763467800bba3df13fc637923b0e58f0bca7c59a
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-10-27 12:11:16 +0900

    コメント追加

--- scene_levelup.h
+++ scene_levelup.h
@@ -5,6 +5,12 @@
 class WeaponLevelUp
 {
 private:
+	// ‰æ‘œ—p•Ï”
+	int img_weapon1;				// •Ší‚P
+	int img_weapon2;				// •Ší‚Q
+	int img_tree_diagram;			// ÷Œ`}
+	int img_cursor;					// ƒJ[ƒ\ƒ‹
+	//int img_background;			// ”wŒi
 
 public:
 	WeaponLevelUp();
