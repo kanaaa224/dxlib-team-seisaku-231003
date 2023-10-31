6b00a82e93681f60649c66289055d51cb3e20dea
kanaaa224 <kanaaa224@outlook.com>
2023-10-31 15:04:50 +0900

    optimize

--- game_ui.cpp
+++ game_ui.cpp
@@ -28,94 +28,212 @@ GameUI::~GameUI() {
 void GameUI::init() {
 	state = 0;
 	frameCounter = 0;
-
-	score  = 0;
-	level  = 0;
-	floor  = 0;
-	hp     = 0;
-	exp    = 0;
-	enemy  = 0;
 };
 
 void GameUI::update(GameScene* gameScene) {
 	frameCounter++;
 
-	hud["opacity"] = 1.0f;
+	hud["opacity"] = 0.5f;
+
+	setScore(432);
+	setLevel(7);
+	setFloor(-2);
+	setHP(900, 1000, 90);
+	setEXP(1500, 2000, 75);
+	setEnemy(234, 2384);
 
 	//score = gameScene->getScore();
-
-	// 仮
-	//weapon["a"];
 };
 
 void GameUI::draw() const {
 	drawHUD();
 };
 
-//void GameUI::drawBackground() const {
-//	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 150);
-//	DrawBox(0, 0, SCREEN_WIDTH, 75, GetColor(0, 0, 0), TRUE);
-//	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-//};
-
-//void GameUI::drawScore() const {
-//	std::string str = "SCORE : " + std::to_string(score);
-//	//DrawStringToHandle(30, 20, str.c_str(), 0x000000, Resources::Get(FONT, FONTTYPE_EIGHTBIT, FONTSIZE_16), 0xffffff);
-//};
-
 void GameUI::drawHUD() const {
-	auto it = hud.find("opacity");
-	if (it != hud.end()) {
-		double opacity = it->second;
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, static_cast<int>(255 * opacity));
-	};
+	/*double opacity = 0.0f;
 
+	auto it = hud.find("opacity");
+	if (it != hud.end()) opacity = it->second;
+
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, static_cast<int>(255 * opacity));*/
+
+
+	//////////////////////////////////////////////////
 	// レベル
+	//////////////////////////////////////////////////
+
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 120);
 	DrawCircle((SCREEN_WIDTH - 60), 60, 40, GetColor(0, 0, 0), true);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-	DrawCircle((SCREEN_WIDTH - 60), 60, 40, GetColor(242, 166, 68), false, 3);
 
+	int color = 0xffffff;
+
+	if (level < 10) {
+		color = GetColor(242, 166, 68);
+	}
+	else if (level < 20) {
+		//
+	};
+
+	DrawCircle((SCREEN_WIDTH - 60), 60, 40, color, false, 3);
+	
 	SetFontSize(50);
 	ChangeFont("Bodoni MT Black Italic", DX_CHARSET_DEFAULT);
 
-	DrawFormatString((SCREEN_WIDTH - 60) - GetDrawFormatStringWidth("2") / 2, 35, 0xffffff, "2");
+	std::string str = std::to_string(level);
+	DrawFormatString((SCREEN_WIDTH - 60) - GetDrawFormatStringWidth(str.c_str()) / 2, 35, 0xffffff, str.c_str());
 
 	SetFontSize(20);
 	ChangeFont("Bernard MT Condensed", DX_CHARSET_DEFAULT);
 
 	DrawFormatString((SCREEN_WIDTH - 90) - GetDrawFormatStringWidth("Lv.") / 2, 70, 0xffffff, "Lv.");
 
-	//ChangeFont("", DX_CHARSET_DEFAULT);
 
+	//////////////////////////////////////////////////
 	// 経験値
+	//////////////////////////////////////////////////
+
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 120);
 	DrawBox((SCREEN_WIDTH - 400), 70, (SCREEN_WIDTH - 110), 90, GetColor(0, 0, 0), true);
-	//DrawCircle((SCREEN_WIDTH - 400), 80, 10, GetColor(0, 0, 0), true);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	DrawBox((SCREEN_WIDTH - 400) + 5, 80 - 5, ((SCREEN_WIDTH - 400) + (290 * 0.8)) - 5, 90 - 5, GetColor(57, 165, 229), true);
+	int current = 0, max = 0, ratio = 0;
 
-	SetFontSize(18);
+	auto it = exp.find("current");
+	if (it != exp.end()) current = it->second;
+
+	it = exp.find("max");
+	if (it != exp.end()) max = it->second;
+
+	it = exp.find("ratio");
+	if (it != exp.end()) ratio = it->second;
+
+	int lx = SCREEN_WIDTH - 400 + 5;
+	int ly = 80 - 5;
+	int rx = lx + static_cast<int>(290 * (ratio / 100.0f)) - 5;
+	int ry = 90 - 5;
+
+	if (!ratio) rx = lx;
+
+	DrawBox(lx, ly, rx, ry, GetColor(57, 165, 229), true);
+
+	SetFontSize(16);
 	ChangeFont("Algerian", DX_CHARSET_DEFAULT);
 
-	DrawFormatString((SCREEN_WIDTH - 395)/* - GetDrawFormatStringWidth("EXP:") / 2*/, 60, 0xffffff, "EXP:");
+	str = "EXP: " + std::to_string(current) + "/" + std::to_string(max);
 
-	ChangeFont("", DX_CHARSET_DEFAULT);
+	DrawFormatString((SCREEN_WIDTH - 395)/* - GetDrawFormatStringWidth(str.c_str()) / 2*/, 60, 0xffffff, str.c_str());
 
+
+	//////////////////////////////////////////////////
 	// HP
+	//////////////////////////////////////////////////
+
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 120);
 	DrawBox(40, 40, 460, 70, GetColor(0, 0, 0), true);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	DrawBox(40 + 5, 40 - 5, ((SCREEN_WIDTH - 400) + (290 * 0.8)) - 5, 90 - 5, GetColor(57, 165, 229), true);
+	current = 0, max = 0, ratio = 0;
 
-	SetFontSize(18);
+	it = hp.find("current");
+	if (it != hp.end()) current = it->second;
+
+	it = hp.find("max");
+	if (it != hp.end()) max = it->second;
+
+	it = hp.find("ratio");
+	if (it != hp.end()) ratio = it->second;
+
+	lx = 40 + 5;
+	ly = 40 + 5;
+	rx = lx + static_cast<int>(420 * (ratio / 100.0f)) - 5;
+	ry = 70 - 5;
+
+	if (!ratio) rx = lx;
+
+	color = GetColor(128, 207, 27);
+	if(ratio <= 10) color = GetColor(255, 0, 0);
+
+	DrawBox(lx, ly, rx, ry, color, true);
+
+	SetFontSize(16);
 	ChangeFont("Algerian", DX_CHARSET_DEFAULT);
 
-	DrawFormatString((SCREEN_WIDTH - 395)/* - GetDrawFormatStringWidth("EXP:") / 2*/, 60, 0xffffff, "EXP:");
+	str = "HP: " + std::to_string(current) + "/" + std::to_string(max);
+
+	DrawFormatString(45, 30, 0xffffff, str.c_str());
+
+
+	//////////////////////////////////////////////////
+	// スコア
+	//////////////////////////////////////////////////
+
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 120);
+	DrawBox(40, 80, 260, 110, GetColor(0, 0, 0), true);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+	SetFontSize(16);
+	ChangeFont("Algerian", DX_CHARSET_DEFAULT);
+
+	if (floor < 0) str = "B" + std::to_string(abs(floor)) + "F";
+	else str = std::to_string(floor) + "F";
+
+	DrawFormatString(50, (SCREEN_HEIGHT - 120), 0xffffff, str.c_str());
+
+
+	//////////////////////////////////////////////////
+	// フロア
+	//////////////////////////////////////////////////
+
+	SetFontSize(38);
+	ChangeFont("Century Gothic Bold Italic", DX_CHARSET_DEFAULT);
+	
+	if(floor < 0) str = "B" + std::to_string(abs(floor)) + "F";
+	else str = std::to_string(floor) + "F";
+
+	DrawFormatString(50, (SCREEN_HEIGHT - 120), 0xffffff, str.c_str());
+
+
+	//////////////////////////////////////////////////
+	// 残りの敵
+	//////////////////////////////////////////////////
+
+	current = 0, max = 0;
+
+	it = enemy.find("current");
+	if (it != enemy.end()) current = it->second;
+
+	it = enemy.find("max");
+	if (it != enemy.end()) max = it->second;
+
+	SetFontSize(24);
+	ChangeFont("", DX_CHARSET_DEFAULT);
+
+	str = "残りの敵: " + std::to_string(current) + "/" + std::to_string(max) + " 体";
+
+	DrawFormatString(50, (SCREEN_HEIGHT - 80), 0xffffff, str.c_str());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	ChangeFont("", DX_CHARSET_DEFAULT);
+
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
 
 	// 現在のスコア を描画
--- game_ui.h
+++ game_ui.h
@@ -8,10 +8,17 @@ class GameUI {
 private:
 	int state, frameCounter;
 
-	int score, level, floor, hp, exp, enemy;
-	std::map<std::string, int> weapon;
+	int score, level, floor;
+
+	std::map<std::string, int> exp;
+	std::map<std::string, int> hp;
+	std::map<std::string, int> enemy;
+
+	//std::vector<std::string, int> weapon;
 
 	std::map<std::string, double> hud;
+
+	std::map<std::string, double> pauseMenu;
 
 public:
 	GameUI();
@@ -45,4 +52,35 @@ public:
 
 	// ゲームクリアアニメーション
 	//void gameOver();
+
+	//////////////////////////////////////////////////
+
+	void setScore(int Score) {
+		score = Score;
+	};
+
+	void setLevel(int Level) {
+		level = Level;
+	};
+
+	void setFloor(int Floor) {
+		floor = Floor;
+	};
+
+	void setEXP(int Current, int Max, int Ratio) {
+		exp["current"] = Current;
+		exp["max"]     = Max;
+		exp["ratio"]   = Ratio;
+	};
+
+	void setHP(int Current, int Max, int Ratio) {
+		hp["current"] = Current;
+		hp["max"]     = Max;
+		hp["ratio"]   = Ratio;
+	};
+
+	void setEnemy(int Current, int Max) {
+		enemy["current"] = Current;
+		enemy["max"]     = Max;
+	};
 };
