2495264bf93497ae62214769e528b9557a0dd1f3
nigirimeshi02 <104492636+nigirimeshi02@users.noreply.github.com>
2023-11-01 11:07:48 +0900

    Update scene_game.cpp

--- scene_game.cpp
+++ scene_game.cpp
@@ -103,19 +103,19 @@ Scene* GameScene::update() {
 	HitCheck();
 
 	//武器と敵の当たり判定
-	////if (stage == 1) {
-	////	for (int i = 0; i < SLIME_1_STAGE_NUM; i++) {
-	////		if (slime[i] != nullptr) {
-	////			if (Weapon->WeaponCollision(slime[i]->GetEnemyLocation(), slime[i]->GetEnemyRadius())) {
-	////				if (slime[i]->GetHitFrameCnt() == 0) {
-	////					slime[i]->SetHitWeaponFlg();
-	////					slime[i]->SetHitHP(Weapon->GetDamage());
-	////					slime[i]->SetHit1stFrameFlg(true);
-	////				}
-	////			}
-	////		}
-	////	}
-	//}
+	if (stage == 1) {
+		for (int i = 0; i < SLIME_1_STAGE_NUM; i++) {
+			if (slime[i] != nullptr) {
+				if (Weapon->WeaponCollision(slime[i]->GetEnemyLocation(), slime[i]->GetEnemyRadius())) {
+					if (slime[i]->GetHitFrameCnt() == 0) {
+						slime[i]->SetHitWeaponFlg();
+						slime[i]->SetHitHP(Weapon->GetDamage());
+						slime[i]->SetHit1stFrameFlg(true);
+					}
+				}
+			}
+		}
+	}
 
 	//武器のレベルアップ（デバッグ用）
 	if (!Weapon->GetLevelUpFlg()) {
