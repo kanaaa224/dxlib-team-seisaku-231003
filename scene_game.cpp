6661ca5c1b5bb274c6003d44a54fbfb9466f474a
kanaaa224 <kanaaa224@outlook.com>
2023-11-08 13:16:43 +0900

    [ GameUI ] new : プレイヤーHP単体表示、武器持ち替え廃止

--- scene_game.cpp
+++ scene_game.cpp
@@ -206,8 +206,7 @@ Scene* GameScene::update() {
 	gameUI->setFloor(-2);
 	gameUI->setEnemy(enemies, SLIME_1_STAGE_NUM);
 
-	if(state) gameUI->setWeapon({ Weapon->GetWeaponType(), Weapon->GetWeaponLevel(), true }, { greatSword, 5, false });
-	else      gameUI->setWeapon({ Weapon->GetWeaponType(), Weapon->GetWeaponLevel(), false }, { greatSword, 5, true });
+	gameUI->setWeapon({ Weapon->GetWeaponType(), Weapon->GetWeaponLevel(), false }, { secondweapon->GetWeaponType(), secondweapon->GetWeaponLevel(), false });
 	//////////////////////////////////////////////////
 	if (enemies <= 0) {
 		gameUI->setBanner("クリア！", "全てのモンスターを倒しました");
@@ -265,6 +264,8 @@ void GameScene::draw() const {
 	else {
 		gameUI->draw();
 		gameUI->drawEnemyHP();
+
+		//gameUI->drawHP();
 	};
 
 	// 武器のレベルアップ画面描画
