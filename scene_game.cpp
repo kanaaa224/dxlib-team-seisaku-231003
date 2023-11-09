ccac3101669042fe174f96fdee94f7036641b3a5
kanaaa224 <kanaaa224@outlook.com>
2023-11-09 09:43:00 +0900

    [ GameMain ] style : コンフリクト回避のため処理を移動

--- scene_game.cpp
+++ scene_game.cpp
@@ -47,200 +47,202 @@ GameScene::~GameScene() {
 Scene* GameScene::update() {
 	if (InputCtrl::GetKeyState(KEY_INPUT_ESCAPE)) return new DebugScene(); // 仮
 
-	if (state) {
-		if (is_map_mode == true) {
-			map->update(is_map_mode);
-			return this;
-		}
+	if (InputCtrl::GetKeyState(KEY_INPUT_P) == PRESS || InputCtrl::GetButtonState(XINPUT_BUTTON_START) == PRESS) {
+		if (state) state = 0;
+		else state++;
+	};
 
-		//武器選択画面
-		if (is_weapon_selct != true)
+	if (!state) return this; // Pause
+
+	//////////////////////////////////////////////////
+
+	if (is_map_mode == true) {
+		map->update(is_map_mode);
+		return this;
+	}
+
+	//武器選択画面
+	if (is_weapon_selct != true)
+	{
+		weapon_selection->update(Weapon, is_weapon_selct);
+		if (is_weapon_selct == true)
 		{
-			weapon_selection->update(Weapon, is_weapon_selct);
-			if (is_weapon_selct == true)
-			{
-				delete weapon_selection;
-				weapon_selection = nullptr;
-			}
-			return this;
+			delete weapon_selection;
+			weapon_selection = nullptr;
 		}
+		return this;
+	}
 
-		// 武器のレベルアップ画面
-		// Xボタンで表示と非表示を切り替え
-		if (InputCtrl::GetButtonState(XINPUT_BUTTON_X) == PRESS)
-		{
-			if (open_level_up)
-			{
-				// 非表示
-				open_level_up = false;
-				restor_cursor_position = true;
-			}
-			else
-			{
-				// 表示
-				open_level_up = true;
-			}
-		}
-
-		// 武器のレベルアップ画面を表示しているときは以下の処理をしない
+	// 武器のレベルアップ画面
+	// Xボタンで表示と非表示を切り替え
+	if (InputCtrl::GetButtonState(XINPUT_BUTTON_X) == PRESS)
+	{
 		if (open_level_up)
 		{
-			weapon_level_up->update(Weapon, secondweapon, restor_cursor_position);
-			return this;
+			// 非表示
+			open_level_up = false;
+			restor_cursor_position = true;
 		}
-
-		//敵//
-		HitCheck();
-		slimeUpdate();
-
-		//武器と敵の当たり判定
-		if (stage == 1) {
-			for (int i = 0; i < SLIME_1_STAGE_NUM; i++) {
-				if (slime[i] != nullptr) {
-					if (Weapon->WeaponCollision(slime[i]->GetEnemyLocation(), slime[i]->GetEnemyRadius())) {
-						if (slime[i]->GetHitFrameCnt() == 0) {
-							slime[i]->SetHitWeaponFlg();
-							slime[i]->SetHitHP(Weapon->GetDamage());
-							slime[i]->SetHit1stFrameFlg(true);
-						}
-					}
-					if (secondweapon->WeaponCollision(slime[i]->GetEnemyLocation(), slime[i]->GetEnemyRadius())) {
-						if (slime[i]->GetHitFrameCnt() == 0) {
-							slime[i]->SetHitWeaponFlg();
-							slime[i]->SetHitHP(Weapon->GetDamage());
-							slime[i]->SetHit1stFrameFlg(true);
-						}
-					}
-				}
-			}
+		else
+		{
+			// 表示
+			open_level_up = true;
 		}
-
-		//武器のレベルアップ（デバッグ用）
-		if (!Weapon->GetLevelUpFlg()) {
-			if (InputCtrl::GetKeyState(KEY_INPUT_1) == PRESS) {
-				Weapon->SetWeaponType(sword);
-			}
-			if (InputCtrl::GetKeyState(KEY_INPUT_2) == PRESS) {
-				Weapon->SetWeaponType(dagger);
-			}
-			if (InputCtrl::GetKeyState(KEY_INPUT_3) == PRESS) {
-				Weapon->SetWeaponType(greatSword);
-			}
-		}
-
-		if (!secondweapon->GetLevelUpFlg()) {
-			if (InputCtrl::GetKeyState(KEY_INPUT_4) == PRESS) {
-				secondweapon->SetWeaponType(spear);
-			}
-			if (InputCtrl::GetKeyState(KEY_INPUT_5) == PRESS) {
-				secondweapon->SetWeaponType(frail);
-			}
-			if (InputCtrl::GetKeyState(KEY_INPUT_6) == PRESS) {
-				secondweapon->SetWeaponType(book);
-			}
-		}
-
-		////////////
-		player->SetLeftTop(backimg->GetStageArray(0));
-		player->SetRightBottom(backimg->GetStageArray(3));
-		backimg->update(player->Player_MoveX(), player->Player_MoveY());
-		player->update();
-		Weapon->Update(player->Player_AimingX(), player->Player_AimingY(), player->Player_Location());
-		Vector tmpV = { player->Player_MoveX(),player->Player_MoveY(),0 };
-		secondweapon->Update(player->Player_AimingX(), player->Player_AimingY(), player->Player_Location(), tmpV);
-		gameUI->update(this);
-
-
-		// ゲームオーバー画面へ遷移
-		if (InputCtrl::GetButtonState(XINPUT_BUTTON_Y) == PRESS) {
-			return new GameOverScene;
-		}
-
-		for (int i = 0; i < MAX_SLIME_NUM; i++) {
-			if (slime[i] != nullptr) {
-				if (slime[i]->GetHitFrameCnt() >= DAMAGE_STOP_FRAME) {
-					slime[i]->SetHit1stFrameFlg(false);
-					slime[i]->SetHitFrameCnt(0);
-				}
-			}
-		}
-		/*if (hitFrameCounter >= DAMAGE_STOP_FRAME) {
-			hitFlg = false;
-			hitFrameCounter = 0;
-		}*/
-
-		for (int i = 0; i < MAX_SLIME_NUM; i++) {
-			if (slime[i] != nullptr) {
-				if (slime[i]->GetHit1stFrameFlg() == true) {
-					slime[i]->hitFrameCntInc();
-				}
-			}
-		}
-		/*if (hitFlg == true) {
-			hitFrameCounter++;
-		}*/
-		frameCounter++;
-
-
-
-
-
-
-		//////////////////////////////////////////////////
-		// GameUI 仮
-		gameUI->setBanner("ミッション（仮）", "全てのモンスターを倒してください");
-
-		int enemies = 0;
-		for (int i = 0; i < SLIME_1_STAGE_NUM; i++) {
-			if (slime[i] != nullptr) enemies++;
-		};
-
-		if ((int)FPSCtrl::Get()) {
-			if (frameCounter % ((int)FPSCtrl::Get() * 2) == 0) exp += 200;
-			if (exp > 2000) {
-				exp = 0;
-				level++;
-			};
-		};
-
-		//gameUI->setScore((SLIME_1_STAGE_NUM - enemies) * 100);
-		gameUI->setHP(player->GetPlayer_HP(), 100, (int)(player->GetPlayer_HP()));
-		gameUI->setEXP(exp, 2000, (exp / 20));
-		gameUI->setLevel(level);
-
-		gameUI->setFloor(-2);
-		gameUI->setEnemy(enemies, SLIME_1_STAGE_NUM);
-
-		gameUI->setWeapon({ Weapon->GetWeaponType(), Weapon->GetWeaponLevel(), false }, { secondweapon->GetWeaponType(), secondweapon->GetWeaponLevel(), false });
-		//////////////////////////////////////////////////
-		if (enemies <= 0) {
-			gameUI->setBanner("クリア！", "全てのモンスターを倒しました");
-			if (gameUI->getState() == 2) {
-				gameUI->init();
-				gameUI->setState(banner);
-			};
-			if (gameUI->getState() == 1) return new GameScene;//is_map_mode = true;
-		};
-		if (player->GetPlayer_HP() <= 0) {
-			gameUI->setBanner("失敗、、", "体力が尽きました、、");
-			if (gameUI->getState() == 2) {
-				gameUI->init();
-				gameUI->setState(banner);
-			};
-			if (gameUI->getState() == 1) return new GameOverScene;
-		};
-		//////////////////////////////////////////////////
-		gameUI->setEnemyHP("魔王 猫スライム", enemies, SLIME_1_STAGE_NUM, enemies * 10); // 怪奇現象発生中
-		//printfDx("%d\n", static_cast<int>((SLIME_1_STAGE_NUM / c) * 100.0f));
-		//printfDx("%f\n", (c / SLIME_1_STAGE_NUM) * 100.0f);
-		//////////////////////////////////////////////////
-
-
-		if (InputCtrl::GetKeyState(KEY_INPUT_P) == PRESS || InputCtrl::GetButtonState(XINPUT_BUTTON_START) == PRESS) state = 0;
 	}
-	else {
-		if (InputCtrl::GetKeyState(KEY_INPUT_P) == PRESS || InputCtrl::GetButtonState(XINPUT_BUTTON_START) == PRESS) state = 1;
+
+	// 武器のレベルアップ画面を表示しているときは以下の処理をしない
+	if (open_level_up)
+	{
+		weapon_level_up->update(Weapon, secondweapon, restor_cursor_position);
+		return this;
+	}
+
+	//敵//
+	HitCheck();
+	slimeUpdate();
+
+	//武器と敵の当たり判定
+	if (stage == 1) {
+		for (int i = 0; i < SLIME_1_STAGE_NUM; i++) {
+			if (slime[i] != nullptr) {
+				if (Weapon->WeaponCollision(slime[i]->GetEnemyLocation(), slime[i]->GetEnemyRadius())) {
+					if (slime[i]->GetHitFrameCnt() == 0) {
+						slime[i]->SetHitWeaponFlg();
+						slime[i]->SetHitHP(Weapon->GetDamage());
+						slime[i]->SetHit1stFrameFlg(true);
+					}
+				}
+				if (secondweapon->WeaponCollision(slime[i]->GetEnemyLocation(), slime[i]->GetEnemyRadius())) {
+					if (slime[i]->GetHitFrameCnt() == 0) {
+						slime[i]->SetHitWeaponFlg();
+						slime[i]->SetHitHP(Weapon->GetDamage());
+						slime[i]->SetHit1stFrameFlg(true);
+					}
+				}
+			}
+		}
+	}
+
+	//武器のレベルアップ（デバッグ用）
+	if (!Weapon->GetLevelUpFlg()) {
+		if (InputCtrl::GetKeyState(KEY_INPUT_1) == PRESS) {
+			Weapon->SetWeaponType(sword);
+		}
+		if (InputCtrl::GetKeyState(KEY_INPUT_2) == PRESS) {
+			Weapon->SetWeaponType(dagger);
+		}
+		if (InputCtrl::GetKeyState(KEY_INPUT_3) == PRESS) {
+			Weapon->SetWeaponType(greatSword);
+		}
+	}
+
+	if (!secondweapon->GetLevelUpFlg()) {
+		if (InputCtrl::GetKeyState(KEY_INPUT_4) == PRESS) {
+			secondweapon->SetWeaponType(spear);
+		}
+		if (InputCtrl::GetKeyState(KEY_INPUT_5) == PRESS) {
+			secondweapon->SetWeaponType(frail);
+		}
+		if (InputCtrl::GetKeyState(KEY_INPUT_6) == PRESS) {
+			secondweapon->SetWeaponType(book);
+		}
+	}
+
+	////////////
+	player->SetLeftTop(backimg->GetStageArray(0));
+	player->SetRightBottom(backimg->GetStageArray(3));
+	backimg->update(player->Player_MoveX(), player->Player_MoveY());
+	player->update();
+	Weapon->Update(player->Player_AimingX(), player->Player_AimingY(), player->Player_Location());
+	Vector tmpV = { player->Player_MoveX(),player->Player_MoveY(),0 };
+	secondweapon->Update(player->Player_AimingX(), player->Player_AimingY(), player->Player_Location(), tmpV);
+	gameUI->update(this);
+
+
+	// ゲームオーバー画面へ遷移
+	if (InputCtrl::GetButtonState(XINPUT_BUTTON_Y) == PRESS) {
+		return new GameOverScene;
+	}
+
+	for (int i = 0; i < MAX_SLIME_NUM; i++) {
+		if (slime[i] != nullptr) {
+			if (slime[i]->GetHitFrameCnt() >= DAMAGE_STOP_FRAME) {
+				slime[i]->SetHit1stFrameFlg(false);
+				slime[i]->SetHitFrameCnt(0);
+			}
+		}
+	}
+	/*if (hitFrameCounter >= DAMAGE_STOP_FRAME) {
+		hitFlg = false;
+		hitFrameCounter = 0;
+	}*/
+
+	for (int i = 0; i < MAX_SLIME_NUM; i++) {
+		if (slime[i] != nullptr) {
+			if (slime[i]->GetHit1stFrameFlg() == true) {
+				slime[i]->hitFrameCntInc();
+			}
+		}
+	}
+	/*if (hitFlg == true) {
+		hitFrameCounter++;
+	}*/
+	frameCounter++;
+
+
+
+
+
+
+	//////////////////////////////////////////////////
+	// GameUI 仮
+	gameUI->setBanner("ミッション（仮）", "全てのモンスターを倒してください");
+
+	int enemies = 0;
+	for (int i = 0; i < SLIME_1_STAGE_NUM; i++) {
+		if (slime[i] != nullptr) enemies++;
 	};
+
+	if ((int)FPSCtrl::Get()) {
+		if (frameCounter % ((int)FPSCtrl::Get() * 2) == 0) exp += 200;
+		if (exp > 2000) {
+			exp = 0;
+			level++;
+		};
+	};
+
+	//gameUI->setScore((SLIME_1_STAGE_NUM - enemies) * 100);
+	gameUI->setHP(player->GetPlayer_HP(), 100, (int)(player->GetPlayer_HP()));
+	gameUI->setEXP(exp, 2000, (exp / 20));
+	gameUI->setLevel(level);
+
+	gameUI->setFloor(-2);
+	gameUI->setEnemy(enemies, SLIME_1_STAGE_NUM);
+
+	gameUI->setWeapon({ Weapon->GetWeaponType(), Weapon->GetWeaponLevel(), false }, { secondweapon->GetWeaponType(), secondweapon->GetWeaponLevel(), false });
+	//////////////////////////////////////////////////
+	if (enemies <= 0) {
+		gameUI->setBanner("クリア！", "全てのモンスターを倒しました");
+		if (gameUI->getState() == 2) {
+			gameUI->init();
+			gameUI->setState(banner);
+		};
+		if (gameUI->getState() == 1) return new GameScene;//is_map_mode = true;
+	};
+	if (player->GetPlayer_HP() <= 0) {
+		gameUI->setBanner("失敗、、", "体力が尽きました、、");
+		if (gameUI->getState() == 2) {
+			gameUI->init();
+			gameUI->setState(banner);
+		};
+		if (gameUI->getState() == 1) return new GameOverScene;
+	};
+	//////////////////////////////////////////////////
+	gameUI->setEnemyHP("魔王 猫スライム", enemies, SLIME_1_STAGE_NUM, enemies * 10); // 怪奇現象発生中
+	//printfDx("%d\n", static_cast<int>((SLIME_1_STAGE_NUM / c) * 100.0f));
+	//printfDx("%f\n", (c / SLIME_1_STAGE_NUM) * 100.0f);
+	//////////////////////////////////////////////////
+
 
 	return this;
 };
