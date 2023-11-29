f762040c90bc940b023a8ff09a12027d8fe1675b
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2023-11-29 14:08:52 +0900

    Update Common.h

--- Common.h
+++ Common.h
@@ -50,7 +50,7 @@
 #define ENEMY_RADIUS 20//敵の半径
 
 //スライム
-#define SLIME_HP_MAX 1000.0f			//HP
+#define SLIME_HP_MAX 10.0f			//HP
 #define SLIME_ATTAK_DAMAGE 1.0f	//ダメージ
 #define MAX_SLIME_NUM 1000		//スライムの最大数
 
