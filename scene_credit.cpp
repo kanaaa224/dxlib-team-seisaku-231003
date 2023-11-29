44b9a5dc56c5650a8eebaa38be53cd7e533a8cd9
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2023-11-29 14:32:32 +0900

    クレジット画面：X座標の変更、エンド画面の時間変更

--- scene_credit.cpp
+++ scene_credit.cpp
@@ -3,7 +3,7 @@
 Credit::Credit()
 {
 	//クレジット画像の読込
-	CreditImage = LoadGraph("resources/images/background.png");
+	CreditImage = LoadGraph("resources/images/Title.png");
 	//時間の初期化
 	g_WaitTime = 0;
 }
@@ -30,18 +30,18 @@ void Credit::draw()const
 	DrawExtendGraph(0, 0,1280,720, CreditImage, TRUE);
 	//使ったサイトや関係者名の名前を表示
 	SetFontSize(50);
-	DrawString(550, 350+g_PosY, "pngtree", 0x000000);
-	DrawString(300, 410+g_PosY, "グラフィックデザイン科の方々", 0x000000);
-	DrawString(550, 470+g_PosY, "チコデザ", 0x000000);
-	DrawString(550, 530+g_PosY, "ツカッテ", 0x000000);
-	DrawString(560, 590+g_PosY, "BOOTH", 0x000000);
-	DrawString(400, 650+g_PosY, "インディー8ビット", 0x000000);
-	DrawString(560, 710+g_PosY, "AKKODiS", 0x000000);
-	DrawString(560, 770+g_PosY, "romptnAI", 0x000000);
-	DrawString(570, 830+g_PosY, "ITOTM", 0x000000);
-	DrawString(500, 890+g_PosY, "くろくまそふと", 0x000000);
-	DrawString(565, 950+g_PosY, "note_lilish", 0x000000);
-	DrawString(455, 1010+g_PosY, "A.I. Art Stock", 0x000000);
-	DrawString(550, 1070+g_PosY, "ドッタウン", 0x000000);
-	DrawString(350, 1130+g_PosY, "ピクセルアートタウン", 0x000000);
+	DrawString(200, 350+g_PosY, "pngtree", 0x000000);
+	DrawString(200, 410+g_PosY, "グラフィックデザイン科の方々", 0x000000);
+	DrawString(200, 470+g_PosY, "チコデザ", 0x000000);
+	DrawString(200, 530+g_PosY, "ツカッテ", 0x000000);
+	DrawString(200, 590+g_PosY, "BOOTH", 0x000000);
+	DrawString(200, 650+g_PosY, "インディー8ビット", 0x000000);
+	DrawString(200, 710+g_PosY, "AKKODiS", 0x000000);
+	DrawString(200, 770+g_PosY, "romptnAI", 0x000000);
+	DrawString(200, 830+g_PosY, "ITOTM", 0x000000);
+	DrawString(200, 890+g_PosY, "くろくまそふと", 0x000000);
+	DrawString(200, 950+g_PosY, "note_lilish", 0x000000);
+	DrawString(200, 1010+g_PosY, "A.I. Art Stock", 0x000000);
+	DrawString(200, 1070+g_PosY, "ドッタウン", 0x000000);
+	DrawString(200, 1130+g_PosY, "ピクセルアートタウン", 0x000000);
 }
