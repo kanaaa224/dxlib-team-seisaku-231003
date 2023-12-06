79b93a53a997d9bc2a3b82dfe55569ef2ca471c6
moyakino <104491816+moyakino@users.noreply.github.com>
2023-12-07 00:06:04 +0900

    関係者の方→スペシャルサンクスに変更

--- scene_credit.cpp
+++ scene_credit.cpp
@@ -22,8 +22,8 @@ Scene* Credit::update()
 	//時間がたてばタイトルへ遷移
 
 	//デバッグ用 あとで消す
-	Credit_Mouse_x = InputCtrl::GetMouseCursor().x;
-	Credit_Mouse_y = InputCtrl::GetMouseCursor().y;
+	//Credit_Mouse_x = InputCtrl::GetMouseCursor().x;
+	//Credit_Mouse_y = InputCtrl::GetMouseCursor().y;
 
 	if (++g_WaitTime < /*1580*/ 2200) {
 
@@ -38,10 +38,8 @@ Scene* Credit::update()
 
 void Credit::draw()const
 {
+	//背景画像
 	DrawGraph(0, 0, CreditImage, TRUE);
-
-	DrawFormatString(0, 300, GetColor(255, 0, 0), "Mouse x %d", g_WaitTime);
-	DrawFormatString(0, 320, GetColor(255, 0, 0), "Mouse y %d", Credit_Mouse_y);
 
 	//　中心線
 	DrawLine(0, 360, 1280, 360, GetColor(255, 0, 0), TRUE);
@@ -49,6 +47,9 @@ void Credit::draw()const
 
 	//使ったサイトや関係者名の名前を表示
 	SetFontSize(50);
+
+	//DrawString(410, 350 + g_PosY, "スペシャルサンクス", 0xffffff);
+
 	DrawString(460, 350 + g_PosY, "使用したサイト", 0xffffff);
 	DrawString(535, 450 + g_PosY, "画像など", 0xffffff);
 	DrawString(470, 550 + g_PosY, "PIXELART TOWN", 0xffffff);
@@ -56,13 +57,13 @@ void Credit::draw()const
 	DrawString(530, 750 + g_PosY, "illustAC", 0xffffff);
 	DrawString(480, 850 + g_PosY, "UTILITY-LABO", 0xffffff);
 	DrawString(480, 950 + g_PosY, "BANNERKOUBOU", 0xffffff);
-	DrawString(500, 1050 + g_PosY, "SE・BGMなど", 0xffffff);
-	DrawString(472, 1150 + g_PosY, "DOVA-SYNDROME", 0xffffff);
-	DrawString(510, 1250 + g_PosY, "ユーフルカ", 0xffffff);
-	DrawString(513, 1350 + g_PosY, "効果音工房", 0xffffff);
-	DrawString(513, 1450 + g_PosY, "効果音ラボ", 0xffffff);
-	DrawString(510, 1550 + g_PosY, "関係者の方", 0xffffff);
-	DrawString(270, 1650 + g_PosY, "グラフィックデザイン科の方々", 0xffffff);
+	DrawString(500, 1150 + g_PosY, "SE・BGMなど", 0xffffff);
+	DrawString(472, 1250 + g_PosY, "DOVA-SYNDROME", 0xffffff);
+	DrawString(510, 1350 + g_PosY, "ユーフルカ", 0xffffff);
+	DrawString(513, 1450 + g_PosY, "効果音工房", 0xffffff);
+	DrawString(513, 1550 + g_PosY, "効果音ラボ", 0xffffff);
+	DrawString(410, 1750 + g_PosY, "スペシャルサンクス", 0xffffff);
+	DrawString(270, 1850 + g_PosY, "グラフィックデザイン科の方々", 0xffffff);
 
 	//DrawString(200, 350+g_PosY, "pngtree", 0xffffff);
 	//DrawString(200, 470+g_PosY, "チコデザ", 0xffffff);
