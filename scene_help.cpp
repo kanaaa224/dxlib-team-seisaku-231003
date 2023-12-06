df562a78fa3e8ee70baaf14df63c8dcfe318ac08
moyakino <104491816+moyakino@users.noreply.github.com>
2023-12-07 04:28:46 +0900

    ヘルプ画面 ステージについて　追加

--- scene_help.cpp
+++ scene_help.cpp
@@ -5,7 +5,8 @@ Help::Help()
 {
 	//ƒwƒ‹ƒv‰æ‘œ‚Ì“Ç‚Ýž‚Ý
 	if (HelpImage1 = LoadGraph("resources/images/Help/Help_Img2.png")) {}
-	if (HelpImage2 = LoadGraph("resources/images/Help/Help2.png")) {}
+	if (HelpImage2 = LoadGraph("resources/images/Help/Help_Stage_Img.png")) {}
+	if (HelpImage3 = LoadGraph("resources/images/Help/skull.png")) {}
 
 	Help_Abtn_Count = 0;
 
@@ -19,24 +20,28 @@ Help::~Help()
 Scene*Help::update()
 {
 	//ƒwƒ‹ƒv2‰æ–Ê‚Ö‘JˆÚ
-	if (InputCtrl::GetButtonState(XINPUT_BUTTON_A) == PRESS)
-	{
-		Help_Abtn_Count++;
-		//return new DrawHelp;
-	}
+	if (InputCtrl::GetButtonState(XINPUT_BUTTON_A) == PRESS) Help_Abtn_Count++;
+
+	//return new DrawHelp;
 	
 	return this;
 }
 
 void Help::draw()const
 {
-	if (Help_Abtn_Count > 0) {
+	if (Help_Abtn_Count == 0) {
 
-		DrawGraph(0, 0, HelpImage2, TRUE);
+		DrawGraph(0, 0, HelpImage1, TRUE);
 	}
-	else {
+	else if(Help_Abtn_Count > 0 && Help_Abtn_Count < 2){
 
 		//ƒwƒ‹ƒv‰æ‘œ‚Ì•\Ž¦
-		DrawGraph(0, 0, HelpImage1, TRUE);
+		DrawGraph(0, 0, HelpImage2, TRUE);
+	}
+	else if (Help_Abtn_Count > 1 && Help_Abtn_Count < 3) {
+
+
+		//ƒwƒ‹ƒv‰æ‘œ‚Ì•\Ž¦
+		DrawGraph(0, 0, HelpImage3, TRUE);
 	}
 }
--- scene_help.h
+++ scene_help.h
@@ -7,6 +7,7 @@ class Help :  public Scene
 private:
 	int HelpImage1;
 	int HelpImage2;
+	int HelpImage3;
 
 	int Help_Abtn_Count;
 
