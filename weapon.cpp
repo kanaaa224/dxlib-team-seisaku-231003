0df29a37532f054d24e0169dba299f270276bb85
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-12-07 15:17:57 +0900

    add

--- weapon.cpp
+++ weapon.cpp
@@ -455,13 +455,13 @@ void weapon::Draw() const
 		DrawCircle(680, 310, 10, 0xff0000, TRUE);
 	}*/
 
-	if (levelUpFlg) {
-		DrawFormatString(450, 60, 0xffffff, "武器をレベルアップします。レベルを入力してください.(0~8)");
-		DrawFormatString(450, 90, 0xffffff, "武器レベル :: %d     Lキーで閉じる",weaponLevel);
-	}
-	else {
-		DrawFormatString(450, 60, 0xffffff, "Lキーでレベルアップメニューを開く(武器１)");
-	}
+	//if (levelUpFlg) {
+	//	DrawFormatString(450, 60, 0xffffff, "武器をレベルアップします。レベルを入力してください.(0~8)");
+	//	DrawFormatString(450, 90, 0xffffff, "武器レベル :: %d     Lキーで閉じる",weaponLevel);
+	//}
+	//else {
+	//	DrawFormatString(450, 60, 0xffffff, "Lキーでレベルアップメニューを開く(武器１)");
+	//}
 
 
 	
