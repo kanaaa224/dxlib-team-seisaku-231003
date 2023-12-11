7e25b2db9c4b7252ec7d5d2530a3da9fc2acf7b6
taiki0627 <104492163+taiki0627@users.noreply.github.com>
2023-12-11 14:13:03 +0900

    map(オープニング、ステージ選択)

--- map.cpp
+++ map.cpp
@@ -13,20 +13,20 @@ Map::Map() {
 	alpha_flg = TRUE;
 
 	// 画像読込
-	if (battle_img == 0) battle_img = (LoadGraph("resources/images/skeleton.png"));
-	if (event_img == 0) event_img = (LoadGraph("resources/images/event.png"));
-	if (rest_img == 0) rest_img = (LoadGraph("resources/images/rest.png"));
-	if (anvil_img == 0) anvil_img = (LoadGraph("resources/images/anvil.png"));
-	if (boss_img == 0) boss_img = (LoadGraph("resources/images/boss.png"));
-	if (icon_back_img == 0) icon_back_img = (LoadGraph("resources/images/icon_back.png"));
-	if (cross_img == 0) cross_img = (LoadGraph("resources/images/cross.png"));
-	if (crown_img == 0) crown_img = (LoadGraph("resources/images/crown.png"));
+	if (battle_img == 0) battle_img = (LoadGraph("resources/images/maps/skeleton.png"));
+	if (event_img == 0) event_img = (LoadGraph("resources/images/maps/event.png"));
+	if (rest_img == 0) rest_img = (LoadGraph("resources/images/maps/rest.png"));
+	if (anvil_img == 0) anvil_img = (LoadGraph("resources/images/maps/anvil.png"));
+	if (boss_img == 0) boss_img = (LoadGraph("resources/images/maps/boss.png"));
+	if (icon_back_img == 0) icon_back_img = (LoadGraph("resources/images/maps/icon_back.png"));
+	if (cross_img == 0) cross_img = (LoadGraph("resources/images/maps/cross.png"));
 	if (roof_img == 0) roof_img = (LoadGraph("resources/images/maps/roof.png"));
 	if (wall_img == 0) wall_img = (LoadGraph("resources/images/maps/wall.png"));
-	if (tower_img == 0) tower_img = (LoadGraph("resources/images/maps/tower.png"));
-	if (map_back_img == 0) map_back_img = (LoadGraph("resources/images/map_back.png"));
+	if (map_back_img == 0) map_back_img = (LoadGraph("resources/images/maps/map_back.png"));
 	if (button_a_image == 0) button_a_image = LoadGraph("resources/images/button_a.png");
 	if (decision_img == 0) decision_img = LoadGraph("resources/images/Logo/UI/logo_dicision.png");
+	if (map_first_img == 0) map_first_img = (LoadGraph("resources/images/maps/map_first.png"));
+	if (stage_select_img == 0) stage_select_img = (LoadGraph("resources/images/maps/stage_select.png"));
 
 	// リザルト画面用
 	battle_count = 0;
@@ -42,21 +42,18 @@ Map::~Map() {
 	DeleteGraph(anvil_img);
 	DeleteGraph(boss_img);
 	DeleteGraph(cross_img);
-	DeleteGraph(crown_img);
 	DeleteGraph(roof_img);
 	DeleteGraph(wall_img);
-	DeleteGraph(tower_img);
 	DeleteGraph(map_back_img);
 	DeleteGraph(button_a_image);
 	DeleteGraph(decision_img);
+	DeleteGraph(map_first_img);
+	DeleteGraph(stage_select_img);
 }
 
 int Map::update(int& mode, int& battleMode, bool& weapon_selected) {
 
 	SoundManager::PlaySoundBGM("bgm_map");
-
-	// アイコン移動距離
-	int icon_vec = 0;
 
 	if (map_move != map_move_log) {
 		if (map_move < 0) map_move = 0;
@@ -68,170 +65,205 @@ int Map::update(int& mode, int& battleMode, bool& weapon_selected) {
 		map_move_log = map_move;
 	}
 
-	// カーソル移動(Lスティック)
-	if (move_cool <= 0) {
-		if (next_stage[pattern][now_stage][1] != -1)
-		{
-			if (InputCtrl::GetStickRatio(L).x >= 0.3) {
-				//カーソルの移動音
-				SoundManager::PlaySoundSE("se_system_select_syu", false);
-				if (cursor_pos + 1 <= 2 && next_stage[pattern][now_stage][cursor_pos + 1] != -1) {
-					cursor_pos++;
-					cursor_loc = next_stage[pattern][now_stage][cursor_pos];
-					move_cool = 15;
+	if (intro_flg == FALSE) {
+		// アイコン移動距離
+		int icon_vec = 0;
+
+		// カーソル移動(Lスティック)
+		if (move_cool <= 0) {
+			if (next_stage[pattern][now_stage][1] != -1)
+			{
+				if (InputCtrl::GetStickRatio(L).x >= 0.3) {
+					//カーソルの移動音
+					SoundManager::PlaySoundSE("se_system_select_syu", false);
+					if (cursor_pos + 1 <= 2 && next_stage[pattern][now_stage][cursor_pos + 1] != -1) {
+						cursor_pos++;
+						cursor_loc = next_stage[pattern][now_stage][cursor_pos];
+						move_cool = 15;
+					}
+					else {
+						cursor_pos = 0;
+						cursor_loc = next_stage[pattern][now_stage][cursor_pos];
+						move_cool = 15;
+					}
+					cursor_move = TRUE;
+					cursor_r = 45;
 				}
-				else {
-					cursor_pos = 0;
-					cursor_loc = next_stage[pattern][now_stage][cursor_pos];
-					move_cool = 15;
-				}
-				cursor_move = TRUE;
-				cursor_r = 45;
-			}
-			else if (InputCtrl::GetStickRatio(L).x <= -0.3) {
-				//カーソルの移動音
-				SoundManager::PlaySoundSE("se_system_select_syu", false);
-				if (cursor_pos - 1 >= 0) {
-					cursor_pos--;
-					cursor_loc = next_stage[pattern][now_stage][cursor_pos];
-					move_cool = 15;
-				}
-				else {
-					for (int i = 2; i > 0; i--) {
-						if (next_stage[pattern][now_stage][i] != -1) {
-							cursor_pos = i;
-							cursor_loc = next_stage[pattern][now_stage][cursor_pos];
-							move_cool = 15;
-							break;
+				else if (InputCtrl::GetStickRatio(L).x <= -0.3) {
+					//カーソルの移動音
+					SoundManager::PlaySoundSE("se_system_select_syu", false);
+					if (cursor_pos - 1 >= 0) {
+						cursor_pos--;
+						cursor_loc = next_stage[pattern][now_stage][cursor_pos];
+						move_cool = 15;
+					}
+					else {
+						for (int i = 2; i > 0; i--) {
+							if (next_stage[pattern][now_stage][i] != -1) {
+								cursor_pos = i;
+								cursor_loc = next_stage[pattern][now_stage][cursor_pos];
+								move_cool = 15;
+								break;
+							}
 						}
 					}
+					cursor_move = TRUE;
+					cursor_r = 45;
 				}
-				cursor_move = TRUE;
-				cursor_r = 45;
 			}
 		}
-	}
-	else if(InputCtrl::GetStickRatio(L).x < 0.3 && InputCtrl::GetStickRatio(L).x > -0.3){
-		move_cool = 0;
-	}
-	else { move_cool--; }
+		else if (InputCtrl::GetStickRatio(L).x < 0.3 && InputCtrl::GetStickRatio(L).x > -0.3) {
+			move_cool = 0;
+		}
+		else { move_cool--; }
 
-	// カーソル移動でカーソルが画面内に収まるようにする
-	if (cursor_move == TRUE) {
-		if (icon_loc[cursor_loc][1] <= 50) {
-			icon_vec = 10;
-		}
-		else if (icon_loc[cursor_loc][1] > SCREEN_HEIGHT - 100) {
-			icon_vec = -10;
-		}
-	}
-
-	// カーソル移動後処理
-	if (cursor_r >= 30) {
-		 {
-			cursor_r = 30;
-		}
-		cursor_r--;
-	}
-
-	if (alpha_flg) {
-		alpha = alpha + 4;
-		if (alpha >= 255) {
-			alpha = 255;
-			alpha_flg = FALSE;
-		}
-	} else {
-		alpha = alpha - 4;
-		if (alpha <= 0) {
-			alpha = 0;
-			alpha_flg = TRUE;
-		}
-	}
-
-	// スクロール(Rスティック)
-	if (InputCtrl::GetStickRatio(R).y >= 0.2 || InputCtrl::GetStickRatio(R).y <= -0.2)
-	{
-		icon_vec = 0;
-		cursor_move = FALSE;
-		// 上スクロール
-		if (icon_loc[data_max - 1][1] < 170) {
-			if (InputCtrl::GetStickRatio(R).y >= 0.2 && InputCtrl::GetStickRatio(R).y < 0.5) {
-				icon_vec = 1;
+		// カーソル移動でカーソルが画面内に収まるようにする
+		if (cursor_move == TRUE) {
+			if (icon_loc[cursor_loc][1] <= 50) {
+				icon_vec = 10;
 			}
-			else if (InputCtrl::GetStickRatio(R).y >= 0.5 && InputCtrl::GetStickRatio(R).y < 0.8) {
-				icon_vec = 3;
-			}
-			else if (InputCtrl::GetStickRatio(R).y >= 0.8) {
-				icon_vec = 5;
+			else if (icon_loc[cursor_loc][1] > SCREEN_HEIGHT - 100) {
+				icon_vec = -10;
 			}
 		}
-		// 下スクロール
-		if (icon_loc[0][1] > SCREEN_HEIGHT - 100) {
-			if (InputCtrl::GetStickRatio(R).y <= -0.2 && InputCtrl::GetStickRatio(R).y > -0.5) {
-				icon_vec = -1;
+
+		// カーソル移動後処理
+		if (cursor_r >= 30) {
+			{
+				cursor_r = 30;
 			}
-			else if (InputCtrl::GetStickRatio(R).y <= -0.5 && InputCtrl::GetStickRatio(R).y > -0.8) {
-				icon_vec = -3;
-			}
-			else if (InputCtrl::GetStickRatio(R).y <= -0.8) {
-				icon_vec = -5;
+			cursor_r--;
+		}
+
+		if (alpha_flg) {
+			alpha = alpha + 4;
+			if (alpha >= 255) {
+				alpha = 255;
+				alpha_flg = FALSE;
 			}
 		}
-	}
+		else {
+			alpha = alpha - 4;
+			if (alpha <= 0) {
+				alpha = 0;
+				alpha_flg = TRUE;
+			}
+		}
 
-	// アイコン移動処理
-	if (icon_vec != 0) {
-		map_move = map_move + icon_vec;
-	}
-	// Aボタンでカーソルのステージに遷移
-	if (InputCtrl::GetButtonState(XINPUT_BUTTON_A) == PRESS || InputCtrl::GetKeyState(KEY_INPUT_RETURN) == PRESS) {
-
-		SoundManager::StopSoundBGM("bgm_map");
-		SoundManager::SetSoundBGMsPosition(0);
-		//カーソルの決定音
-		SoundManager::PlaySoundSE("se_system_normal_decision", false);
-
-		now_stage = cursor_loc;
-
-		switch (MapData[now_stage])
+		// スクロール(Rスティック)
+		if (InputCtrl::GetStickRatio(R).y >= 0.2 || InputCtrl::GetStickRatio(R).y <= -0.2)
 		{
-		case 0:		//戦闘
-			if (weapon_selected) mode = GameSceneMode::main;
-			else mode = GameSceneMode::weaponSelect;
-
-			battleMode = GameSceneBattleMode::normal;
-			battle_count++;
-			break;
-		case 1:		//イベント（中ボス）
-			if (weapon_selected) mode = GameSceneMode::main;
-			else mode = GameSceneMode::weaponSelect;
-
-			battleMode = GameSceneBattleMode::midBoss;
-			event_count++;
-			break;
-		case 2:		//休憩
-			mode = GameSceneMode::rest;
-			ClearStage();
-			rest_count++;
-			break;
-		case 3:		//鍛冶屋
-			mode = GameSceneMode::blacksmith;
-			ClearStage();
-			anvil_count++;
-			break;
-		case 4:		//ボス
-			if (weapon_selected) mode = GameSceneMode::main;
-			else mode = GameSceneMode::weaponSelect;
-
-			battleMode = GameSceneBattleMode::boss;
-			ClearStage();
-			boss_count++;
-			break;
-		default:
-			break;
+			icon_vec = 0;
+			cursor_move = FALSE;
+			// 上スクロール
+			if (icon_loc[data_max - 1][1] < 170) {
+				if (InputCtrl::GetStickRatio(R).y >= 0.2 && InputCtrl::GetStickRatio(R).y < 0.5) {
+					icon_vec = 1;
+				}
+				else if (InputCtrl::GetStickRatio(R).y >= 0.5 && InputCtrl::GetStickRatio(R).y < 0.8) {
+					icon_vec = 3;
+				}
+				else if (InputCtrl::GetStickRatio(R).y >= 0.8) {
+					icon_vec = 5;
+				}
+			}
+			// 下スクロール
+			if (icon_loc[0][1] > SCREEN_HEIGHT - 100) {
+				if (InputCtrl::GetStickRatio(R).y <= -0.2 && InputCtrl::GetStickRatio(R).y > -0.5) {
+					icon_vec = -1;
+				}
+				else if (InputCtrl::GetStickRatio(R).y <= -0.5 && InputCtrl::GetStickRatio(R).y > -0.8) {
+					icon_vec = -3;
+				}
+				else if (InputCtrl::GetStickRatio(R).y <= -0.8) {
+					icon_vec = -5;
+				}
+			}
 		}
 
-		return 0;
+		// アイコン移動処理
+		if (icon_vec != 0) {
+			map_move = map_move + icon_vec;
+		}
+		if (select_draw > 0) {
+			if (select_draw > 150) {
+				select_loc = select_loc + 4;
+			}
+			else if (select_draw <= 30){
+				select_loc = select_loc - 4;
+			}
+			select_draw--;
+		}
+		// Aボタンでカーソルのステージに遷移
+		if (no_input_time <= 0) {
+			if (InputCtrl::GetButtonState(XINPUT_BUTTON_A) == PRESS || InputCtrl::GetKeyState(KEY_INPUT_RETURN) == PRESS) {
+
+				SoundManager::StopSoundBGM("bgm_map");
+				SoundManager::SetSoundBGMsPosition(0);
+				//カーソルの決定音
+				SoundManager::PlaySoundSE("se_system_normal_decision", false);
+
+				now_stage = cursor_loc;
+
+				switch (MapData[now_stage])
+				{
+				case 0:		//戦闘
+					if (weapon_selected) mode = GameSceneMode::main;
+					else mode = GameSceneMode::weaponSelect;
+
+					battleMode = GameSceneBattleMode::normal;
+					break;
+				case 1:		//イベント（中ボス）
+					if (weapon_selected) mode = GameSceneMode::main;
+					else mode = GameSceneMode::weaponSelect;
+
+					battleMode = GameSceneBattleMode::midBoss;
+					break;
+				case 2:		//休憩
+					mode = GameSceneMode::rest;
+					ClearStage();
+					break;
+				case 3:		//鍛冶屋
+					mode = GameSceneMode::blacksmith;
+					ClearStage();
+					break;
+				case 4:		//ボス
+					if (weapon_selected) mode = GameSceneMode::main;
+					else mode = GameSceneMode::weaponSelect;
+
+					battleMode = GameSceneBattleMode::boss;
+					ClearStage();
+					break;
+				default:
+					break;
+				}
+			}
+
+			return 0;
+		}
+		else
+		{
+			no_input_time--;
+		}
+	}
+	else
+	{
+		if (map_move < 440) {
+			map_move = map_move + 5;
+		}
+		else {
+			if (intro_time > 0) {
+				intro_time--;
+			}
+			else
+			{
+				intro_flg = FALSE;
+				map_move = 0;
+				select_draw = 180;
+				select_loc = -120;
+			}
+		}
+		
 	}
 };
 
@@ -252,21 +284,21 @@ void Map::draw() const {
 		for (int j = 0; next_stage[pattern][i][j] > 0 && j <= 2; j++) {
 			int next_loc = next_stage[pattern][i][j];
 			if (stage_log[log_i] == i && stage_log[log_i + 1] == next_loc) {
-				DrawLineAA(icon_loc_center[i][0], icon_loc_center[i][1],
-					icon_loc_center[next_loc][0], icon_loc_center[next_loc][1], 0xFFD000, 5);
+				DrawLineAA((float)icon_loc_center[i][0], (float)icon_loc_center[i][1],
+					(float)icon_loc_center[next_loc][0], (float)icon_loc_center[next_loc][1], 0xFFD000, 5);
 				log_i++;
 			}
 			else if (i == now_stage && j == cursor_pos) {
 
-				DrawLineAA(icon_loc_center[i][0], icon_loc_center[i][1],
-					icon_loc_center[next_loc][0], icon_loc_center[next_loc][1], GetColor(255, 208 + (alpha * (255 - 208) / 255), alpha), 9.0f - ((float)alpha * 4 / 255));
+				DrawLineAA((float)icon_loc_center[i][0], (float)icon_loc_center[i][1],
+					(float)icon_loc_center[next_loc][0], (float)icon_loc_center[next_loc][1], GetColor(255, 208 + (alpha * (255 - 208) / 255), alpha), 9.0f - ((float)alpha * 4 / 255));
 			}
 			else {
-				DrawLineAA(icon_loc_center[i][0], icon_loc_center[i][1],
-					icon_loc_center[next_loc][0], icon_loc_center[next_loc][1], 0x888888, 5);
+				DrawLineAA((float)icon_loc_center[i][0], (float)icon_loc_center[i][1],
+					(float)icon_loc_center[next_loc][0], (float)icon_loc_center[next_loc][1], 0x888888, 5);
 			}
 		}
-			DrawGraph(icon_loc[i][0] - 5, icon_loc[i][1] - 5, icon_back_img, TRUE);
+		DrawGraph(icon_loc[i][0] - 5, icon_loc[i][1] - 5, icon_back_img, TRUE);
 		// アイコン表示
 		switch (MapData[i]) {
 		case 0:
@@ -296,6 +328,19 @@ void Map::draw() const {
 		// カーソル表示(アイコンの円と被るように半径に-1)
 		DrawCircle(icon_loc_center[cursor_loc][0], icon_loc_center[cursor_loc][1], cursor_r - 1, 0x050505, 0, 5);
 		DrawCircle(icon_loc_center[cursor_loc][0], icon_loc_center[cursor_loc][1], cursor_r - 1, 0xFFD000, 0, 3);
+
+		// フォント表示
+		if (select_draw > 0) {
+			DrawBox(435, 45 + select_loc, 890, 120 + select_loc, 0x000000, 1);
+			SetFontSize(64);
+			DrawString(440, 50 + select_loc, "マップを選べ！", 0xfffffff);
+		}
+		if (intro_flg)
+		{
+			/*SetFontSize(64);
+			DrawString(340, -380 + map_move, "最上階の魔王を倒せ！", 0x0000a0);*/
+			DrawGraph(340, -380 + map_move, map_first_img, 1);
+		}
 
 		// Aボタン
 		DrawGraph(1150, 650, button_a_image, TRUE);
@@ -354,9 +399,11 @@ void Map::ResetStage() {
 		icon_loc[i][1] = icon_loc_def[pattern][i][1];
 		icon_loc_center[i][0] = icon_loc_def[pattern][i][0] + 25;
 		icon_loc_center[i][1] = icon_loc_def[pattern][i][1] + 25;
-		map_move = 0;
-		map_move_log = 0;
 	}
+	map_move = 0;
+	map_move_log = 0;
+	intro_flg = TRUE;
+	intro_time = 180;
 }
 
 void Map::SetStage(int st_min, int st_max, int rand_min, int rand_max, int type ,int data_num) {
